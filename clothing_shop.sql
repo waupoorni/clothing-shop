a66fe8de61e77262f19bbcda9671c1422d202484
Udara Poorni <udarapoorni2004@gmail.com>
2025-09-20 17:12:08 +0530

    Update clothing_shop.sql

--- clothing_shop.sql
+++ clothing_shop.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Sep 18, 2025 at 12:08 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Host: 127.0.0.1
+-- Generation Time: Sep 20, 2025 at 09:47 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -41,7 +41,7 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `name`, `username`, `email`, `password_hash`, `created_at`) VALUES
-(1, 'sandalu buddima', 'sandalu', 'sandalu@gmail.com', '$2y$10$MUCEhgkDbkbITc3JjEfj6eH5rONu2B5AvHdq8C45aM.86vS50YC1C', '2025-09-18 06:33:07');
+(2, 'Udara Poorni', 'waupoorni', 'udarapoorni2004@gmail.com', '$2y$10$Oc4iIfphpJXguCe./2AnsOG8hhOr/2U1nb6ez4ppWgjasFT2pN56a', '2025-09-18 13:15:59');
 
 -- --------------------------------------------------------
 
@@ -61,6 +61,18 @@ CREATE TABLE `clothes` (
   `description` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `clothes`
+--
+
+INSERT INTO `clothes` (`id`, `name`, `sku`, `price`, `category`, `size`, `stock`, `image_url`, `description`, `created_at`) VALUES
+(4, 'Summer frock', '21', 4000.00, 'women', 'S,M,L,Xl', 20, '/clothing-shop/uploads/clothes/1758203547_aec188.jpg', 'A frock is a stylish dress, often worn by women and girls, that combines comfort with elegance.', '2025-09-18 13:52:27'),
+(5, 'CharmWear', '25', 3500.00, 'women', 's', 10, '/clothing-shop/uploads/clothes/1758203662_058639.jpg', 'It is usually designed with soft fabrics like cotton, silk, or chiffon for casual or formal wear.', '2025-09-18 13:54:22'),
+(6, 'Floral Party Frock', '6', 2000.00, 'kids', 's', 10, '/clothing-shop/uploads/clothes/1758203877_175c3b.jpg', 'Floral Party Frock – A bright, colorful frock with floral prints, perfect for birthdays and special occasions.', '2025-09-18 13:57:57'),
+(7, 'Polka Cutie', '5', 1500.00, 'kids', 'M', 5, '/clothing-shop/uploads/clothes/1758204051_8030af.jpg', 'Polka Dot Frock – A cute frock with polka dot patterns, stylish yet simple for outings.', '2025-09-18 14:00:51'),
+(8, 'FlexiShirt', '33', 4000.00, 'men', 'L', 20, '/clothing-shop/uploads/clothes/1758204166_e6db06.jpg', 'Slim-fit shirt with a modern look.', '2025-09-18 14:02:46'),
+(9, 'Classic Blue', '34', 5000.00, 'men', 'L', 20, '/clothing-shop/uploads/clothes/1758204282_8554ed.jpg', 'Popularly used in jeans and jackets.', '2025-09-18 14:04:42');
 
 -- --------------------------------------------------------
 
@@ -82,7 +94,9 @@ CREATE TABLE `contact_messages` (
 --
 
 INSERT INTO `contact_messages` (`id`, `name`, `email`, `subject`, `message`, `created_at`) VALUES
-(1, 'sandalu buddima', 'sandalu@gmail.com', 'hi hi', 'i want to buy', '2025-09-18 06:22:43');
+(2, 'Yasurika', 'yasurika@gmail.com', 'Order Request – Blue Denim', 'Do you have ripped denim jeans in size 32? If yes, I’d like to place an order.', '2025-09-18 14:12:29'),
+(3, 'bhagya', 'bagya@gmail.com', 'Return/Exchange Request', 'I received my order yesterday but would like to exchange it for a different size. Can you help me with the process?', '2025-09-18 14:28:43'),
+(4, 'Senura', 'senura@gmail.com', 'Size Availability Check', 'Do you have black denim in size 32? If yes, I’d like to place an order.', '2025-09-18 14:29:40');
 
 --
 -- Indexes for dumped tables
@@ -117,19 +131,19 @@ ALTER TABLE `contact_messages`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `clothes`
 --
 ALTER TABLE `clothes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `contact_messages`
 --
 ALTER TABLE `contact_messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
